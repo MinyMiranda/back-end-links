dfb8b90fe52c50c9831eebd8811a6392f7fd14b5
ysmine <ysminemiranda@gmail.com>
2020-12-19 03:06:48 -0300

    :construction: Controller index :construction:

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Link;
+
+class HomeController extends Controller
+{
+
+    /**
+     * Retorna todos os Links Juntamento com os links filhos
+     *  
+     * @return string JSON
+     */
+    public function index()
+    {
+        return response()->json(
+            Link::getLink()
+        );
+    }
+}
---
+++ app/LinkClass.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use App\Models\Link;
+
+class LinkClass
+{
+
+}
