f1c3714689297364c7c53716a524f1676ffbaf75
ysmine <ysminemiranda@gmail.com>
2020-12-19 11:42:20 -0300

    :construction: Class Link create e update construction:

---
+++ app/Classes/LinkClass.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Classes;
+
+use App\Models\Link;
+use Illuminate\Support\Facades\Validator;
+
+class LinkClass
+{
+
+    /**
+     * Cria um novo Link
+     *  
+     * @param Illuminate\Http\Request  $request
+     * @return string JSON
+     */
+    public function createLink($request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'url' => 'required|string',
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()], 400);
+        }
+        $author = Link::create($request->all());
+        return response()->json(['message'=>'Sucesso ao criar'], 201);
+    }
+
+    /**
+     * Edita um Link já existente
+     *  
+     * @param Illuminate\Http\Request  $request
+     * @return string JSON
+     */
+    public function updateLink($id, $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'url' => 'required|string',
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()], 400);
+        }
+        $author = Link::find($id);
+        $author->update($request->all());
+        return response()->json(['message' => 'Succeso ao editar!'], 200);
+    }
+}
